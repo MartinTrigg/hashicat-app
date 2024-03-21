8820f533b185b9f30a66fd890bd0927f5e1af4a6
MartinTrigg <martin.trigg@bupa.com>
2024-03-21 13:30:49 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -200,6 +200,7 @@ resource "null_resource" "configure-cat-app" {
       host     = azurerm_public_ip.catapp-pip.fqdn
     }
   }
+  
 }
 module "backupstorage" {
   source = "app.terraform.io/MartinTriggBupa/azure-backup/azurerm"
